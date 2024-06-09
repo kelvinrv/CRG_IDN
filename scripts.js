512fc62f3e6519d05a15c41eebed990831c6f423
kelvinrv <kelvin.ramos@ib.edu.ar>
2024-06-09 17:01:16 -0300

    updating

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function toggleContent(id) {
+    var contenido = document.getElementById(id);
+    if (contenido.style.display === "none") {
+        contenido.style.display = "block";
+    } else {
+        contenido.style.display = "none";
+    }
+}
+function openMenu() {
+    // Aquí puedes agregar el código para abrir el menú o realizar otra acción
+    console.log("Haz hecho clic en el iframe.");
+}
