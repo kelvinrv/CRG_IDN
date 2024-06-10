3715fbc97e2fade02dae393740e41f5be5ce7fc2
kelvinrv <kelvin.ramos@ib.edu.ar>
2024-06-10 07:43:34 -0300

    al dar click en ver más muestra el contenido oculto

---
+++ songs/ministracion/show.js
@@ -0,0 +1,14 @@
+document.getElementById("show-more").onclick = function() {
+    var dots = document.getElementsByClassName("hidden-content");
+    var showMore = document.getElementById("show-more");
+
+    for (var i = 0; i < dots.length; i++) {
+        if (dots[i].style.display === "none") {
+            dots[i].style.display = "inline";
+            showMore.innerHTML = "Mostrar menos";
+        } else {
+            dots[i].style.display = "none";
+            showMore.innerHTML = "Mostrar más";
+        }
+    }
+};
